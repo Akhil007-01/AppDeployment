df1514576fd9e903107a71ab01f4889333d54a2f
root <root@ip-10-1-1-225.ec2.internal>
2022-01-17 11:22:24 +0000

    content changed

--- src/Contact.js
+++ src/Contact.js
@@ -8,7 +8,7 @@ class Contact extends Component {
         <p>The easiest thing to do is post on
         our <a href="https://docs.google.com/document/d/1wnYdfvV-RPM3314hZAS7WRRR4oRWiKbb_uwv1vpOtiQ/edit?usp=sharing">forums</a>.
         </p>
-		<p>If there is something that you want to talk about! Contact us at <b>+91 9538299368</b></p>
+		<p>If there is something that you need to ask about feel free to  Contact us at <b>+91 9538299369</b></p>
       </div>
     );
   }
